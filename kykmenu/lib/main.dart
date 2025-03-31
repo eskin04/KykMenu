34bb1cc6b2e044c55b11220920a9e99f86c69d81
okan eskin <71434594+eskin04@users.noreply.github.com>
2025-03-31 20:44:21 +0300

    .

--- kykmenu/lib/main.dart
+++ kykmenu/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart'; // Yeni dosyanı ekledik
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -11,6 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginScreen(), // Burada ayrı bir dosyaya taşıdık
-    );
-  }
+    );
+  }
 }
