532540184f318003fdfe69631c884025c4a3426c
okan eskin <71434594+eskin04@users.noreply.github.com>
2025-04-03 21:21:14 +0300

    .

--- kykmenu/lib/main.dart
+++ kykmenu/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:kykmenu/service/messaging.dart';
 import 'screens/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:intl/date_symbol_data_local.dart'; // Bunu ekle!
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:kykmenu/screens/welcome_screen.dart'; // Bunu ekle!
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +20,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Bir hata oluştu!'));
+          } else if (snapshot.hasData) {
+            return WelcomeScreen();
+          } else {
+            return LoginScreen();
+          }
+        },
+      ),
+    );
   }
 }
